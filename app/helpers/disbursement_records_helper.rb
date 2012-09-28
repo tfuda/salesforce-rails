edcf42977fb9e9786dfe86bdbf4511cf15e4228d
Tom Fuda <tom@9summer.com>
2012-09-28 11:23:28 -0400

    Hide Download link until status is "Done".

--- app/helpers/disbursement_records_helper.rb
+++ app/helpers/disbursement_records_helper.rb
@@ -1,7 +1,7 @@
 module DisbursementRecordsHelper
 
   def pdf_image_tag(filename, options = {})
-    path = Rails.root.join("app/assets/images/#{filename}")
+    path = Rails.root.join("assets/images/#{filename}")
     options[:src] = path.to_s
     attributes = options.map{ |k,v| "#{k}='#{v}'" }.join(" ")
     raw("<img #{attributes} />")
